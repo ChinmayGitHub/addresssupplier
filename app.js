8bddd537bc6fa05fc7b8e903b224a72e175648dc
Chinmay Samant <chinmaysamant@MacBook-Pro-16.local>
2021-05-01 13:59:44 +0530

    2 commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2');
  
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'mysql',
   user: 'root',
   password: 'root123'
 });
